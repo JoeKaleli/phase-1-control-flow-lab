9eced8d28debbf2b6451a47c58a9245962ed903c
JoeKaleli <kalelimburu9@gmail.com>
2023-12-14 12:33:39 +0300

    Done with assinment

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(i){
+  // Write your code here!\
+  let greeting;
+
+
+  if (i<=400) {
+    greeting = "This one is on me!";
+  } else if (i>200 && i<= 2500) {
+    greeting = "I will gladly take your thirty bucks.";
+  } else if (i>2500) {
+    greeting = "No can do.";
+  }
+  return greeting;
+
 }
 
-function ternaryCheckCity(){
+
+function ternaryCheckCity(destination){
   // Write your code here!
+const result =
+ destination === "NYC" ? 'Ok, sounds good.' : "No go.";
+ return result;
+
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tipAmount){
   // Write your code here!
-}
+  
+    
+    switch (true) {
+      case tipAmount === 'generous':
+        return 'Thank you so much.';
+      case tipAmount === 'not as generous':
+        return 'Thank you.';
+      default:
+        return 'Bye.';
+    }
+  }
+  
+
+console.log(switchOnCharmFromTip("not as generous"))
